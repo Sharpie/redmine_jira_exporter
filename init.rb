7155906e51be53deeccc5509ab1476a53b92a7f4
Charlie Sharpsteen <source@sharpsteen.net>
2013-09-11 17:15:34 -0700

    Initial commit
    Add project skeloton generated by:
    
        RAILS_ENV=production script/rails generate redmine_plugin redmine_jira_exporter

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :redmine_jira_exporter do
+  name 'Redmine JIRA Exporter plugin'
+  author 'Charlie Sharpsteen'
+  description 'This plugin exports selected issues from Redmine to JIRA.'
+  version '0.0.1'
+end
