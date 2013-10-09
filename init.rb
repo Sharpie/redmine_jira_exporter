df22a3ecbaf6a51cc07b37a6d45e7f9909aec9aa
Charlie Sharpsteen <source@sharpsteen.net>
2013-10-09 15:31:56 -0700

    Abort gracefully if settings.yaml is not present
    If `settings.yaml` can't be loaded during startup, gracefully abort the plugin
    and log an error.
    
    Fixes GH-1.

--- init.rb
+++ init.rb
@@ -2,19 +2,24 @@ require 'redmine_jira_exporter'
 
 # Redmine doesn't have an obvious way to do per-project settings. So, as a
 # workaround we load from a local `settings.yaml` file.
-RedmineJIRAExporter.settings = YAML.load_file File.join(File.dirname(__FILE__), 'settings.yaml')
+RedmineJIRAExporter.settings = YAML.load_file File.join(File.dirname(__FILE__), 'settings.yaml') rescue nil
 
-# The File.basename hackery is so we can rename the plugin directory to
-# something like `redmine_zzz_jira_exporter` this causes all plugin hooks to be
-# fired last which puts content at the end of views instead of in the middle.
-Redmine::Plugin.register File.basename(File.dirname(__FILE__)).intern do
-  name 'Redmine JIRA Exporter plugin'
-  author 'Charlie Sharpsteen'
-  description 'This plugin exports selected issues from Redmine to JIRA.'
-  version '0.0.1'
 
-  project_module :jira_export do
-    permission :view_jira_exports, {:jira_export => :view}
-    permission :create_jira_exports, {:jira_export => :create}
+if RedmineJIRAExporter.settings.nil?
+  Rails.logger.error "jira_export_plugin: Failed to load settings file: #{File.join(File.dirname(__FILE__))}"
+else
+  # The File.basename hackery is so we can rename the plugin directory to
+  # something like `redmine_zzz_jira_exporter` this causes all plugin hooks to be
+  # fired last which puts content at the end of views instead of in the middle.
+  Redmine::Plugin.register File.basename(File.dirname(__FILE__)).intern do
+    name 'Redmine JIRA Exporter plugin'
+    author 'Charlie Sharpsteen'
+    description 'This plugin exports selected issues from Redmine to JIRA.'
+    version '0.0.1'
+
+    project_module :jira_export do
+      permission :view_jira_exports, {:jira_export => :view}
+      permission :create_jira_exports, {:jira_export => :create}
+    end
   end
 end
